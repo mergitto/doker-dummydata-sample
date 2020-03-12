2e4f5795282dae915938b273feb8481c491fc828
mergitto <merugitosu@gmail.com>
2020-03-12 23:59:29 +0900

    add name

--- initial.sql
+++ initial.sql
@@ -17,7 +17,9 @@ CREATE TABLE `sample`.`child`
   `id` INT NOT NULL AUTO_INCREMENT,
   `parent_id` INT NOT NULL,
   `age` INT NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`, `parent_id`),
+  INDEX `idx_name` (`name` ASC),
   INDEX `idx_parent_id` (`parent_id` ASC),
   CONSTRAINT `fk_parent_id`
     FOREIGN KEY (`parent_id`)
@@ -28,33 +30,31 @@ CREATE TABLE `sample`.`child`
 
 -- INSERT parent 指数関数的にデータを増やす
 BEGIN;
-INSERT INTO `sample`.`parent` (`id`,`count`) VALUES (1, CEIL(RAND() * 100));
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-SELECT sleep(1);
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-SELECT sleep(1);
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-SELECT sleep(1);
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
-INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 100) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`id`,`count`) VALUES (1, CEIL(RAND() * 1000));
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`; /* 50万 */
+INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`; /* 100万 */
+/* INSERT INTO `sample`.`parent` (`count`) select CEIL(RAND() * 1000) from `sample`.`parent`;*/ /* 200万ぐらいからめっちゃ時間かかるからコメントアウト */
 COMMIT;
 
 -- INSERT child
 BEGIN;
-INSERT INTO `sample`.`child` (`parent_id`, `age`) select id, 10 from sample.parent ORDER BY RAND();
+INSERT INTO `sample`.`child` (`parent_id`, `age`, `name`) select id, CEIL(RAND() * 100), SUBSTRING(MD5(RAND()), 1, 10) from sample.parent ORDER BY RAND();
 COMMIT;
